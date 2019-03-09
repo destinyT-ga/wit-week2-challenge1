b5bc33fa6c18ad8bfaad4e4f795c2898636d9bd9
Destiny T-ga <destinygladys@gmail.com>
2019-03-09 12:36:40 +0300

    first commit

---
+++ dictionary1.py
@@ -0,0 +1,5 @@
+listA = range(1, 16) 
+dictA = {} 
+for x in listA: 
+    dictA[x] = x*x 
+print(dictA) 
---
+++ minor_youth_elder.py
@@ -0,0 +1,13 @@
+dob=int(input("year of birth: "))
+# print(dob)
+age=2019-dob
+# print("this is your age: ",age)
+if age<=18:
+    print("you are a minor")
+if age>18 and age<=36:
+    print("you are a youth")
+if age>36:
+    print("you are an elder") 
+
+
+
